a315e24c5446b51a2acb715dfc180321c0e443f6
Marcus <marcus.gamboa@gmail.com>
2021-02-13 16:41:24 -0700

    Completed Heading Section.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React from "react";
-import logo from "./logo.svg";
+import reactLogo from "./images/logo.svg";
+import reduxLogo from "./images/redux.svg";
+import tailwindLogo from "./images/tailwind.svg";
 import "./App.css";
+
+import Editor from "./Components/editor.js";
+
+const styles = {
+  base: "",
+  //Heading Section
+  headingSection: "flex flex-col items-center bg-gray-300 w-screen",
+  h1: "text-5xl sm:text-7xl text-center mt-5",
+  //Subheading
+  subHeading: "mt-8 font-medium",
+  logos: "max-h-16 inline text-center",
+  tailwind: "ml-2",
+};
 
 class App extends React.Component {
   render() {
-    const heading = "flex flex-col align-center w-max";
-    const h1 = "text-7xl";
-    const logos = "max-h-16 inline text-center";
     return (
-      <div>
-        <div className="heading">
-          <h1 className={h1}>MarkDown Previewer</h1>
-          <p>
-            Utilizing
-            <img src={logo} alt="React Logo" width="60px" className={logos} />
+      <div className={styles.base}>
+        <div className={styles.headingSection}>
+          <h1 className={styles.h1}>MarkDown Previewer</h1>
+          <p className={styles.subHeading}>
+            Utilizing:
+            <img
+              src={reactLogo}
+              alt="React Logo"
+              width="50px"
+              className={styles.logos}
+            />
+            <img
+              src={reduxLogo}
+              alt="Redux Logo"
+              width="30px"
+              className={styles.logos}
+            />
+            <img
+              src={tailwindLogo}
+              alt="Tailwind CSS Logo"
+              width="30px"
+              className={`${styles.logos} ${styles.tailwind}`}
+            />
           </p>
         </div>
+        <Editor />
       </div>
     );
   }
---
+++ src/Components/editor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const styles = {
+  editorContainer: "border-solid",
+};
+
+class Editor extends React.Component {
+  render() {
+    return <div>hi</div>;
+  }
+}
+
+export default Editor;
